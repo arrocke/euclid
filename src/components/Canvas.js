ea7e86abbefd4979719d64807e80331889a968fc
Adrian Rocke <business@adrianrocke.com>
2019-03-08 16:00:42 -0600

    click to add points

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -5,9 +5,14 @@ class Canvas extends Component {
   constructor(props) {
     super (props)
     this.resize = this.resize.bind(this)
+    this.onCanvasClick = this.onCanvasClick.bind(this)
     this.state = {
       width: window.innerWidth,
-      height: window.innerHeight
+      height: window.innerHeight,
+      elements: [
+        { type: 'point', x: 100, y: -200 },
+        { type: 'point', x: -100, y: 100 }
+      ]
     }
   }
 
@@ -16,9 +21,38 @@ class Canvas extends Component {
     const height = this.state.height
     const x = -width / 2
     const y = -height / 2
-    return <svg width="100%" height="100%" viewBox={`${x} ${y} ${width} ${height}`} >
-      <Point x={100} y={-200} />
+
+    const elements = this.state.elements.map((el, i) => {
+      switch (el.type) {
+        case 'point':
+          return <Point
+            key={i}
+            x={el.x} y={el.y}
+            elementId={i}
+          />
+        default:
+          return ''
+      }
+    })
+
+    return <svg
+      width="100%" height="100%"
+      viewBox={`${x} ${y} ${width} ${height}`}
+      onClick={this.onCanvasClick}
+    >
+      {elements}
     </svg>
+  }
+
+  onCanvasClick(e) {
+    const x = e.clientX - this.state.width / 2
+    const y = -(e.clientY - this.state.height / 2)
+    this.setState({
+      elements: [
+        ...this.state.elements,
+        { type: 'point', x, y }
+      ]
+    })
   }
 
   componentDidMount () {
--- src/components/Point.js
+++ src/components/Point.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 
 class Point extends Component {
+  constructor(props) {
+    super(props)
+    this.onClick = this.onClick.bind(this)
+    this.onMouseOver = this.onMouseOver.bind(this)
+    this.onMouseOut = this.onMouseOut.bind(this)
+  }
+
   render () {
-    return <circle cx={this.props.x} cy={-this.props.y} r="4" />
+    return <circle
+      cx={this.props.x} cy={-this.props.y}
+      r="4"
+      onClick={this.onClick}
+      onMouseOver={this.onMouseOver}
+      onMouseOut={this.onMouseOut}
+    />
+  }
+
+  eventData () {
+    const { elementId, x, y } = this.props
+    return { elementId, x, y }
+  }
+
+  onClick () {
+    this.props.onClick && this.props.onClick(this.eventData())
+  }
+
+  onMouseOver () {
+    this.props.onMouseOver && this.props.onMouseOver(this.eventData())
+  }
+
+  onMouseOut () {
+    this.props.onMouseOut && this.props.onMouseOut(this.eventData())
   }
 }
 
