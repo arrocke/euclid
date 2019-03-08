ba8115c4cc17fb91e1b5dbe969e38a72036a6b62
Adrian Rocke <business@adrianrocke.com>
2019-03-07 20:20:44 -0600

    set up svg canvas

---
+++ src/components/Canvas.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Canvas extends Component {
+  constructor(props) {
+    super (props)
+    this.resize = this.resize.bind(this)
+    this.state = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+  }
+
+  render () {
+    const width = this.state.width
+    const height = this.state.height
+    const x = -width / 2
+    const y = -height / 2
+    return <svg width="100%" height="100%" viewBox={`${x} ${y} ${width} ${height}`} />
+  }
+
+  componentDidMount () {
+    window.addEventListener('resize', this.resize)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.resize)
+  }
+
+  resize () {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+}
+
+export default Canvas
