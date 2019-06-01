dd00b72a736d2014cb0520c30cdb2af4b4961a73
Adrian Rocke <business@adrianrocke.com>
2019-06-01 13:37:35 -0500

    autofix for ts/tsx, and tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,14 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      }
     ],
   },
   plugins: [
