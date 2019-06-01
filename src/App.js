4e7878f160ce03b44a97074e45d6f391e605fae7
Adrian Rocke <business@adrianrocke.com>
2019-06-01 00:00:13 -0500

    reset again...

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PageView from './components/PageView'
-
-function App () {
-  return <PageView />
-}
-
-export default App;
--- src/views/HomeView.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function HomeView () {
-  return <div class="HomeView"></div>
-}
-
-export default HomeView
